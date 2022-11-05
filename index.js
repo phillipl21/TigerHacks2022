588c4ffd0d45ba5c3b323b43ae56a356352803c5
CandymanAndy <74220837+AndrewChang-cpu@users.noreply.github.com>
2022-11-05 13:20:20 -0500

    Introduce marker functionality

--- index.js
+++ index.js
@@ -101,6 +101,38 @@ function highlightRoutes(map) {
   );
 }
 
+function loadMarkers(map) {
+  const iconBase =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
+
+  const icons = {
+    parking: {
+      icon: iconBase + "parking_lot_maps.png",
+    },
+    library: {
+      icon: iconBase + "library_maps.png",
+    },
+    info: {
+      icon: iconBase + "info-i_maps.png",
+    },
+  };
+
+  const features = [
+    {
+      position: new google.maps.LatLng(38.944261, -92.326639),
+      type: "parking",
+    },
+  ];
+
+  for (let i = 0; i < features.length; i++) {
+    const marker = new google.maps.Marker({
+      position: features[i].position,
+      icon: icons[features[i].type].icon,
+      map: map,
+    });
+  }
+}
+
 function initialize() {
   var map = new google.maps.Map(document.getElementById("map_canvas"), {
     center: new google.maps.LatLng(38.944261, -92.326639),
@@ -112,13 +144,23 @@ function initialize() {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     styles: [
       {
-        featureType: "poi",
-        stylers: [{ visibility: "simplified" }],
+        featureType: "poi.business",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "poi.school",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "poi.sports_complex",
+        stylers: [{ visibility: "off" }],
       },
     ],
   });
 
   highlightRoutes(map);
+
+  loadMarkers(map);
 
   // map.setOptions();
 }
