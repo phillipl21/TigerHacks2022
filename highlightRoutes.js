5e3317a6e2aee3501d027601c34e4773eadcd05b
Phillip Lei <plgrx@umsystem.edu>
2022-11-05 13:56:28 -0500

    separates highlightroutes() into a separate js file

---
+++ highlightRoutes.js
@@ -0,0 +1,140 @@
+export function highlightRoutes(map) {
+    console.log("fuk");
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer({
+      map: map,
+      preserveViewport: true,
+    });
+    directionsService.route(
+      {
+        origin: new google.maps.LatLng(38.9404, -92.3277),
+        destination: new google.maps.LatLng(38.944716, -92.332159),
+        waypoints: [
+          {
+            stopover: false,
+            location: new google.maps.LatLng(38.946456, -92.325635),
+          },
+        ],
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      function (response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+          // directionsDisplay.setDirections(response);
+          var polyline = new google.maps.Polyline({
+            path: [],
+            strokeColor: "#0000FF",
+            strokeWeight: 5,
+            strokeOpacity: 0.5,
+          });
+          var bounds = new google.maps.LatLngBounds();
+  
+          var legs = response.routes[0].legs;
+          for (let i = 0; i < legs.length; i++) {
+            var steps = legs[i].steps;
+            for (let j = 0; j < steps.length; j++) {
+              var nextSegment = steps[j].path;
+              for (let k = 0; k < nextSegment.length; k++) {
+                polyline.getPath().push(nextSegment[k]);
+                bounds.extend(nextSegment[k]);
+              }
+            }
+          }
+  
+          polyline.setMap(map);
+          google.maps.event.addListener(polyline, "click", () => {
+            console.log("polyline 1 clicked");
+          });
+        } else {
+          window.alert("Directions request failed due to " + status);
+        }
+      }
+    );
+    // -------------------------------------------------------------------
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer({
+      map: map,
+      preserveViewport: true,
+    });
+    directionsService.route(
+      {
+        origin: new google.maps.LatLng(38.942377, -92.329996),
+        destination: new google.maps.LatLng(38.940321, -92.323739),
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      function (response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+          // directionsDisplay.setDirections(response);
+          var polyline = new google.maps.Polyline({
+            path: [],
+            strokeColor: "#FF00FF",
+            strokeWeight: 5,
+            strokeOpacity: 0.5,
+          });
+          var bounds = new google.maps.LatLngBounds();
+  
+          var legs = response.routes[0].legs;
+          for (let i = 0; i < legs.length; i++) {
+            var steps = legs[i].steps;
+            for (let j = 0; j < steps.length; j++) {
+              var nextSegment = steps[j].path;
+              for (let k = 0; k < nextSegment.length; k++) {
+                polyline.getPath().push(nextSegment[k]);
+                bounds.extend(nextSegment[k]);
+              }
+            }
+          }
+  
+          polyline.setMap(map);
+          google.maps.event.addListener(polyline, "click", () => {
+            console.log("polyline 2 clicked");
+          });
+        } else {
+          window.alert("Directions request failed due to " + status);
+        }
+      }
+    );
+    /***** HEARNES LOOP 401 *****/
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer({
+      map: map,
+      preserveViewport: true,
+    });
+    directionsService.route(
+      {
+        origin: new google.maps.LatLng(38.942377, -92.329996),
+        destination: new google.maps.LatLng(38.940321, -92.323739),
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      function (response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+          // directionsDisplay.setDirections(response);
+          var polyline = new google.maps.Polyline({
+            path: [],
+            strokeColor: "#FF00A",
+            strokeWeight: 5,
+            strokeOpacity: 0.5,
+          });
+          var bounds = new google.maps.LatLngBounds();
+  
+          var legs = response.routes[0].legs;
+          for (let i = 0; i < legs.length; i++) {
+            var steps = legs[i].steps;
+            for (let j = 0; j < steps.length; j++) {
+              var nextSegment = steps[j].path;
+              for (let k = 0; k < nextSegment.length; k++) {
+                polyline.getPath().push(nextSegment[k]);
+                bounds.extend(nextSegment[k]);
+              }
+            }
+          }
+  
+          polyline.setMap(map);
+          google.maps.event.addListener(polyline, "click", () => {
+            console.log("polyline 3 clicked");
+          });
+        } else {
+          window.alert("Directions request failed due to " + status);
+        }
+      }
+    );
+  }
--- index.js
+++ index.js
@@ -1,105 +1,11 @@
+import { highlightRoutes } from "./highlightRoutes.js";
+
 const BOUNDS = {
   north: 38.955,
   south: 38.935,
   west: -92.35,
   east: -92.3,
 };
-
-function highlightRoutes(map) {
-  var directionsService = new google.maps.DirectionsService();
-  var directionsDisplay = new google.maps.DirectionsRenderer({
-    map: map,
-    preserveViewport: true,
-  });
-  directionsService.route(
-    {
-      origin: new google.maps.LatLng(38.9404, -92.3277),
-      destination: new google.maps.LatLng(38.944716, -92.332159),
-      waypoints: [
-        {
-          stopover: false,
-          location: new google.maps.LatLng(38.946456, -92.325635),
-        },
-      ],
-      travelMode: google.maps.TravelMode.DRIVING,
-    },
-    function (response, status) {
-      if (status === google.maps.DirectionsStatus.OK) {
-        // directionsDisplay.setDirections(response);
-        var polyline = new google.maps.Polyline({
-          path: [],
-          strokeColor: "#0000FF",
-          strokeWeight: 5,
-          strokeOpacity: 0.5,
-        });
-        var bounds = new google.maps.LatLngBounds();
-
-        var legs = response.routes[0].legs;
-        for (i = 0; i < legs.length; i++) {
-          var steps = legs[i].steps;
-          for (j = 0; j < steps.length; j++) {
-            var nextSegment = steps[j].path;
-            for (k = 0; k < nextSegment.length; k++) {
-              polyline.getPath().push(nextSegment[k]);
-              bounds.extend(nextSegment[k]);
-            }
-          }
-        }
-
-        polyline.setMap(map);
-        google.maps.event.addListener(polyline, "click", () => {
-          console.log("polyline 1 clicked");
-        });
-      } else {
-        window.alert("Directions request failed due to " + status);
-      }
-    }
-  );
-  // -------------------------------------------------------------------
-  var directionsService = new google.maps.DirectionsService();
-  var directionsDisplay = new google.maps.DirectionsRenderer({
-    map: map,
-    preserveViewport: true,
-  });
-  directionsService.route(
-    {
-      origin: new google.maps.LatLng(38.942377, -92.329996),
-      destination: new google.maps.LatLng(38.940321, -92.323739),
-      travelMode: google.maps.TravelMode.DRIVING,
-    },
-    function (response, status) {
-      if (status === google.maps.DirectionsStatus.OK) {
-        // directionsDisplay.setDirections(response);
-        var polyline = new google.maps.Polyline({
-          path: [],
-          strokeColor: "#FF00FF",
-          strokeWeight: 5,
-          strokeOpacity: 0.5,
-        });
-        var bounds = new google.maps.LatLngBounds();
-
-        var legs = response.routes[0].legs;
-        for (i = 0; i < legs.length; i++) {
-          var steps = legs[i].steps;
-          for (j = 0; j < steps.length; j++) {
-            var nextSegment = steps[j].path;
-            for (k = 0; k < nextSegment.length; k++) {
-              polyline.getPath().push(nextSegment[k]);
-              bounds.extend(nextSegment[k]);
-            }
-          }
-        }
-
-        polyline.setMap(map);
-        google.maps.event.addListener(polyline, "click", () => {
-          console.log("polyline 2 clicked");
-        });
-      } else {
-        window.alert("Directions request failed due to " + status);
-      }
-    }
-  );
-}
 
 function loadMarkers(map) {
   const iconBase =
