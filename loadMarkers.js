bc0979fb2bf1d88ebca19e80e7069f8fa4c93e8a
Phillip Lei <plgrx@umsystem.edu>
2022-11-05 15:48:02 -0500

    all parking symbols added

--- loadMarkers.js
+++ loadMarkers.js
@@ -32,7 +32,7 @@ export function loadMarkers(map) {
         type: "parking",
       },
       { //tiger ave garage
-        position: new google.maps.LatLng(38.93882162105329, -92.32788552293911),
+        position: new google.maps.LatLng(38.93831466093758, -92.33017375768632),
         type: "parking",
       },
       { //virginia avenue garage
@@ -41,6 +41,42 @@ export function loadMarkers(map) {
       },
       { //parking structure #7
         position: new google.maps.LatLng(38.93708661633961, -92.32506965841958),
+        type: "parking",
+      },
+      { //parking structure #7
+        position: new google.maps.LatLng(38.93708661633961, -92.32506965841958),
+        type: "parking",
+      },
+      { //WG10 - near Twain
+        position: new google.maps.LatLng(38.94454794767063, -92.33127226676486),
+        type: "parking",
+      },
+      { //Memorial Union Visitor Lot
+        position: new google.maps.LatLng(38.94546999814113, -92.3241858704455),
+        type: "parking",
+      },
+      { //WC7 - near Middlebush
+        position: new google.maps.LatLng(38.94577039208177, -92.32654621440473),
+        type: "parking",
+      },
+      { //CG17 - between SC & Stank
+        position: new google.maps.LatLng(38.941889532937616, -92.32941135766158),
+        type: "parking",
+      },
+      { //CG2 - black culture center
+        position: new google.maps.LatLng(38.94091110831369, -92.32426151634915),
+        type: "parking",
+      },
+      { //VAG Surface Parking
+        position: new google.maps.LatLng(38.93992641151287, -92.3238752782632),
+        type: "parking",
+      },
+      { //Southwest Athletic lot
+        position: new google.maps.LatLng(38.937654, -92.331345),
+        type: "parking",
+      },
+      { //SC / Hitt Street visitor lot
+        position: new google.maps.LatLng(38.94327220119762, -92.32583671801366),
         type: "parking",
       },
     ];
