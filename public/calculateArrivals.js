0e76b50da822dbce985a2f8289d97057ee5337a6
CandymanAndy <74220837+AndrewChang-cpu@users.noreply.github.com>
2022-11-06 07:47:49 -0600

    Improvements to calculate arrivals

--- public/calculateArrivals.js
+++ public/calculateArrivals.js
@@ -5,24 +5,43 @@ const minutes = date.getMinutes();
 
 function calculateHearnes() {
   let element = document.getElementById("arrivals-1");
-  if (hours < 20 || hours > 4) {
+  if (hours < 20 && hours > 4) {
     if (minutes < 20) {
-      element.innerHTML += "<br>" + (hours % 12).toString() + ":20 - Hearnes";
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":30 - Student Center";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":20 - Hearnes";
+      element.innerHTML +=
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":30 - Student Center";
     } else if (minutes < 30) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":30 - Student Center";
-      element.innerHTML += "<br>" + (hours % 12).toString() + ":50 - Hearnes";
-    } else if (minutes < 50) {
-      element.innerHTML += "<br>" + (hours % 12).toString() + ":50 - Hearnes";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":30 - Student Center";
       element.innerHTML +=
-        "<br>" + ((hours + 1) % 12).toString() + ":00 - Student Center";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":50 - Hearnes";
+    } else if (minutes < 50) {
+      element.innerHTML +=
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":50 - Hearnes";
+      element.innerHTML +=
+        "<br>" +
+        ((hours + 1) % 12 !== 0 ? (hours + 1) % 12 : 12).toString() +
+        ":00 - Student Center";
     } else {
       element.innerHTML +=
-        "<br>" + ((hours + 1) % 12).toString() + ":00 - Student Center";
+        "<br>" +
+        ((hours + 1) % 12 !== 0 ? (hours + 1) % 12 : 12).toString() +
+        ":00 - Student Center";
       element.innerHTML +=
-        "<br>" + ((hours + 1) % 12).toString() + ":20 - Hearnes";
+        "<br>" +
+        ((hours + 1) % 12 !== 0 ? (hours + 1) % 12 : 12).toString() +
+        ":20 - Hearnes";
     }
   } else if (hours === 4 && minutes > 50) {
     element.innerHTML += "<br>5:00 - Student Center";
@@ -35,42 +54,70 @@ function calculateHearnes() {
 
 function calculateTrowbridge() {
   let element = document.getElementById("arrivals-2");
-  if (hours < 20 || hours > 5) {
+  if (hours < 20 && hours > 5) {
     if (minutes < 5) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":05 - Trowbridge";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":05 - Trowbridge";
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":10 - University Hospital";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":10 - University Hospital";
     } else if (minutes < 10) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":10 - University Hospital";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":10 - University Hospital";
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":15 - Student Center";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":15 - Student Center";
     } else if (minutes < 15) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":15 - Student Center";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":15 - Student Center";
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":35 - Trowbridge";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":35 - Trowbridge";
     } else if (minutes < 35) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":35 - Trowbridge";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":35 - Trowbridge";
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":40 - University Hospital";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":40 - University Hospital";
     } else if (minutes < 40) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":40 - University Hospital";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":40 - University Hospital";
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":45- Student Center";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":45- Student Center";
     } else if (minutes < 45) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":45- Student Center";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":45- Student Center";
       element.innerHTML +=
-        "<br>" + ((hours + 1) % 12).toString() + ":05 - Trowbridge";
+        "<br>" +
+        ((hours + 1) % 12 !== 0 ? (hours + 1) % 12 : 12).toString() +
+        ":05 - Trowbridge";
     } else {
       element.innerHTML +=
-        "<br>" + ((hours + 1) % 12).toString() + ":05 - Trowbridge";
+        "<br>" +
+        ((hours + 1) % 12 !== 0 ? (hours + 1) % 12 : 12).toString() +
+        ":05 - Trowbridge";
       element.innerHTML +=
-        "<br>" + ((hours + 1) % 12).toString() + ":10 - University Hospital";
+        "<br>" +
+        ((hours + 1) % 12 !== 0 ? (hours + 1) % 12 : 12).toString() +
+        ":10 - University Hospital";
     }
   } else if (hours <= 5) {
     element.innerHTML += "<br>6:05 - Towbridge<br>6:10 - University Hospital";
@@ -79,25 +126,43 @@ function calculateTrowbridge() {
 
 function calculateReactor() {
   let element = document.getElementById("arrivals-3");
-  if (hours < 20 || hours > 5) {
+  if (hours < 20 && hours > 5) {
     if (minutes < 20) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":20 - 6th & Stewart";
-      element.innerHTML += "<br>" + (hours % 12).toString() + ":30 - Reactor";
-    } else if (minutes < 30) {
-      element.innerHTML += "<br>" + (hours % 12).toString() + ":30 - Reactor";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":20 - 6th & Stewart";
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":50 - 6th & Stewart";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":30 - Reactor";
+    } else if (minutes < 30) {
+      element.innerHTML +=
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":30 - Reactor";
+      element.innerHTML +=
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":50 - 6th & Stewart";
     } else if (minutes < 50) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":50 - 6th & Stewart";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":50 - 6th & Stewart";
       element.innerHTML +=
-        "<br>" + ((hours + 1) % 12).toString() + ":00 - Reactor";
+        "<br>" +
+        ((hours + 1) % 12 !== 0 ? (hours + 1) % 12 : 12).toString() +
+        ":00 - Reactor";
     } else {
       element.innerHTML +=
-        "<br>" + ((hours + 1) % 12).toString() + ":00 - Reactor";
+        "<br>" +
+        ((hours + 1) % 12 !== 0 ? (hours + 1) % 12 : 12).toString() +
+        ":00 - Reactor";
       element.innerHTML +=
-        "<br>" + ((hours + 1) % 12).toString() + ":20 - 6th & Stewart";
+        "<br>" +
+        ((hours + 1) % 12 !== 0 ? (hours + 1) % 12 : 12).toString() +
+        ":20 - 6th & Stewart";
     }
   } else if (hours <= 5) {
     element.innerHTML += "<br>6:00 - Reactor";
@@ -108,52 +173,95 @@ function calculateCampus() {
   let element = document.getElementById("arrivals-4");
   if ((hours === 0 && minutes < 15) || hours > 19) {
     if (minutes < 5) {
-      element.innerHTML += "<br>" + (hours % 12).toString() + ":05 - Hearnes";
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":10 - Trowbridge";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":05 - Hearnes";
+      element.innerHTML +=
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":10 - Trowbridge";
     } else if (minutes < 10) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":10 - Trowbridge";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":10 - Trowbridge";
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":15 - Hitt & Cherry";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":15 - Hitt & Cherry";
     } else if (minutes < 15) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":15 - Hitt & Cherry";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":15 - Hitt & Cherry";
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":20 - 6th & Stewart";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":20 - 6th & Stewart";
     } else if (minutes < 20) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":20 - 6th & Stewart";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":20 - 6th & Stewart";
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":30 - Student Center";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":30 - Student Center";
     } else if (minutes < 30) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":30 - Student Center";
-      element.innerHTML += "<br>" + (hours % 12).toString() + ":35 - Hearnes";
-    } else if (minutes < 35) {
-      element.innerHTML += "<br>" + (hours % 12).toString() + ":35 - Hearnes";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":30 - Student Center";
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":40 - Trowbridge";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":35 - Hearnes";
+    } else if (minutes < 35) {
+      element.innerHTML +=
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":35 - Hearnes";
+      element.innerHTML +=
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":40 - Trowbridge";
     } else if (minutes < 40) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":40 - Trowbridge";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":40 - Trowbridge";
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":45 - Hitt & Cherry";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":45 - Hitt & Cherry";
     } else if (minutes < 45) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":45 - Hitt & Cherry";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":45 - Hitt & Cherry";
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":50 - 6th & Stewart";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":50 - 6th & Stewart";
     } else if (minutes < 50) {
       element.innerHTML +=
-        "<br>" + (hours % 12).toString() + ":50 - 6th & Stewart";
+        "<br>" +
+        (hours % 12 !== 0 ? hours % 12 : 12).toString() +
+        ":50 - 6th & Stewart";
       element.innerHTML +=
-        "<br>" + ((hours + 1) % 12).toString() + ":00 - Trowbridge";
+        "<br>" +
+        ((hours + 1) % 12 !== 0 ? (hours + 1) % 12 : 12).toString() +
+        ":00 - Trowbridge";
     } else {
       element.innerHTML +=
-        "<br>" + ((hours + 1) % 12).toString() + ":00 - Trowbridge";
+        "<br>" +
+        ((hours + 1) % 12 !== 0 ? (hours + 1) % 12 : 12).toString() +
+        ":00 - Trowbridge";
       element.innerHTML +=
-        "<br>" + ((hours + 1) % 12).toString() + ":05 - Hearnes";
+        "<br>" +
+        ((hours + 1) % 12 !== 0 ? (hours + 1) % 12 : 12).toString() +
+        ":05 - Hearnes";
     }
   } else if (hours <= 19) {
     element.innerHTML += "<br>8:00 - Trowbridge";
